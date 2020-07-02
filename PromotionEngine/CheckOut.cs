fb9078b2b8407a454a33110307318b45fd72ec53
S <shankara.swamy@philips.com>
2020-07-02 18:51:00 +0530

    applying promotion

--- PromotionEngine/CheckOut.cs
+++ PromotionEngine/CheckOut.cs
@@ -17,17 +17,42 @@ namespace PromotionEngine {
             }
 
             string[] checkOutList = checkOutInput.Split(new[] { ' ' });
-            Dictionary<string, string> unitsVsItems = new Dictionary<string, string>();
+            Dictionary<string, string> checkOutValues = new Dictionary<string, string>();
             foreach (var item in checkOutList) {
                 string[] vals = item.Split(new[] { ' ' });
-                unitsVsItems.Add(vals[1], vals[0]);
+                checkOutValues.Add(vals[1], vals[0]);
+            }
+
+
+            string[] promorules = activePromotion.Split(new[] { ';' });
+            List<Tuple<string, int, double>> promotion = new List<Tuple<string, int, double>>();
+            foreach (var promorule in promorules) {
+                string[] chunks2 = promorule.Split(new[] { ' ' });
+
+                promotion.Add(new Tuple<string, int, double>(chunks2[1],
+                    Convert.ToInt16(chunks2[0]),
+                    Convert.ToDouble(chunks2[2])));
             }
 
             double total = 0;
-            foreach (var items in unitsVsItems.Keys) {
-                total += Convert.ToDouble(prices[items]) * Convert.ToDouble(unitsVsItems[items]);
+            foreach (var item in checkOutValues.Keys) {
+                double price = ApplyPromotion(promotion, item, Convert.ToDouble(prices[item]));
+                double unit = Convert.ToDouble(checkOutValues[item]);
+
+                total += unit * price;
             }
             return 0;
+        }
+
+        private double ApplyPromotion(List<Tuple<string, int, double>> promotion,
+            string item, double v) {
+            foreach (var tuple in promotion) {
+                if (tuple.Item1.Contains(item)) {
+                    return tuple.Item3;
+                }
+            }
+
+            return v;
         }
     }
 }
