bb7083b152ce5e680ddf81d90d43b3c9e05d4846
S <shankara.swamy@philips.com>
2020-07-02 18:33:44 +0530

    implementation

--- PromotionEngine/CheckOut.cs
+++ PromotionEngine/CheckOut.cs
@@ -5,14 +5,28 @@ using System.Text;
 namespace PromotionEngine {
     public class CheckOut {
 
-        /// <summary>
-        /// Ctor
-        /// </summary>
-        public CheckOut() {
+        public double GetTotal(string faceValue,
+            string activePromotion,
+            string checkOutInput) {
 
-        }
+            string[] chunks = faceValue.Split(new[] { ';' });
+            Dictionary<string, string> prices = new Dictionary<string, string>();
+            foreach (var chunk in chunks) {
+                string[] vals = chunk.Split(new[] { ' ' });
+                prices.Add(vals[1], vals[0]);
+            }
 
-        public double GetTotal(string faceValue, string activePromotion, string checkOutInput) {
+            string[] checkOutList = checkOutInput.Split(new[] { ' ' });
+            Dictionary<string, string> unitsVsItems = new Dictionary<string, string>();
+            foreach (var item in checkOutList) {
+                string[] vals = item.Split(new[] { ' ' });
+                unitsVsItems.Add(vals[1], vals[0]);
+            }
+
+            double total = 0;
+            foreach (var items in unitsVsItems.Keys) {
+                total += Convert.ToDouble(prices[items]) * Convert.ToDouble(unitsVsItems[items]);
+            }
             return 0;
         }
     }
--- UnitTests/PromotionEnginTests.cs
+++ UnitTests/PromotionEnginTests.cs
@@ -10,16 +10,32 @@ namespace UnitTests {
         /// 
         /// </summary>
         [Test]
-        public void DefaultPromotion() {
+        public void Scenario1() {
             string checkOutInput = "1A 2B 3C";
             string faceValues = "50 A;30 B;20 C;15 D";
-            string activePromotion = "3 A 130;2 B 45;2 CD 30";
+            string activePromotion = "3 A 130;2 B 45;1 CD 30";
             CheckOut sut = new CheckOut();
             double expectedTotal = 155; //50 + 45 + 60 
 
             double actualTotal = sut.GetTotal(faceValues, activePromotion, checkOutInput);
 
             Assert.IsTrue(expectedTotal==actualTotal, $"it should have been{expectedTotal}");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void Scenario2() {
+            string checkOutInput = "6 A;4 B;3 C;2 D";
+            string faceValues = "50 A;30 B;20 C;15 D";
+            string activePromotion = "3 A 130;2 B 45;2 CD 30";
+            CheckOut sut = new CheckOut();
+            double expectedTotal = 430; //260 + 90 + 80 
+
+            double actualTotal = sut.GetTotal(faceValues, activePromotion, checkOutInput);
+
+            Assert.IsTrue(expectedTotal == actualTotal, $"it should have been{expectedTotal}");
         }
     }
 }
